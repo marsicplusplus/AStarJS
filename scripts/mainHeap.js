8173d6fd27081cd1f45d6242accfda01a4d220f5
lorenzomarsicano <lorenzomarsicano@gmail.com>
2018-03-08 22:07:39 +0100

    Comments

--- scripts/mainHeap.js
+++ scripts/mainHeap.js
@@ -1,41 +1,64 @@
+// For the Graphic Object for the background
 var bg;
+// For the Canvas ref
 var canvas;
 
+// Size of the grid in term of cells
 var gridSize;
+// Size of the cells in term of pixels
 var cellSize;
-var weight = 1;
+// The grid structure
 var grid;
 
+// Boolean for checking if the bg need to be drawn (the first time)
 var drawBG = true;
+// Boolean for checking if the bg will be drawn or not
 var bgToggle = true;
+// Boolean for checking is the algorithm has started
 var running = false;
+// Boolean for checking if the algorithm has failed
 var failed = false;
+// Boolean for checking if the algorithm has started
 var started = false;
 
+// The OpenSet, managed by a min-heap
 var frontiera;
+// The ClosedSet, managed as an array
 var esplorati;
+// An array containing the obstacles
 var ostacoli;
+// The start cell
 var start;
+// The end cell
 var end;
+// The last cell that has been considered in the algorithm
 var last;
 
 
 function setup() {
+	// Create the canvas and attach it to the div
 	canvas = createCanvas(800, 800)
 	canvas.parent("p5Canvas");
+	// Create the graphic for the bg and set it density to 1
 	bg = createGraphics(800, 800);
 	bg.pixelDensity(1);
+	// Set the framerate to regulate the speed of the simulation
 	frameRate(30);  
-    gridSize = 30;
+	// Number of cells in the grid
+	gridSize = 30;
+	// Size of one cell
 	cellSize = canvas.width / gridSize;
+	// Array of obstacles
 	ostacoli = [];
+	
 	start = new Cell(0, 0);
 	end = new Cell(gridSize - 1, gridSize - 1);
     grid = initArray();
 	frontiera = new BinaryHeap(function(cell){
         return cell.f;
 	});
-    esplorati = [];
+	esplorati = [];
+	// Add the start position to the OpenSet
 	frontiera.push(start);
 }
 
@@ -158,6 +181,7 @@ function algorithm(){
 }
 
 function heuristic(cell, goal){
+	// MD
 	return Math.abs(goal.x - cell.x) + Math.abs(goal.y - cell.y);
 	//return dist(cell.x, cell.y, goal.x, goal.y)
 }
@@ -173,7 +197,7 @@ function initArray(){
 				cl[j] = end;
 			else
 				cl[j] = new Cell(i, j);
-			if(random(1) < 0.4 && ((i != start.x && j != start.y) || (i != end.x && j != end.y))){
+			if(random(1) < 0.4 && cl[j] != start && cl[j] != end){
 				cl[j].wall = true;
 				ostacoli.push(cl[j]);
 			}
